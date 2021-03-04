20ebaa1ba1a01c56ade3336efed6a6397e911774
jinsunee <jsunee.dev@gmail.com>
2021-03-04 18:49:11 +0900

    add - typescript 설정

--- config/common.js
+++ config/common.js
@@ -18,18 +18,19 @@ module.exports = {
         })
     ],
     resolve: {
-        extensions: ['.js', '.jsx']
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
     },
     module: {
         rules: [
             {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            },
-            {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
                 use: 'babel-loader'
+            },
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: /node_modules/,
+                use: 'ts-loader'
             }
         ]
     }
---
+++ src/components/Button.tsx
@@ -0,0 +1,15 @@
+import React, { MouseEventHandler } from 'react';
+
+type Props = {
+  onClick: MouseEventHandler,
+  text: string,
+}
+
+const Button = ({ onClick, text }: Props) => (
+    <button onClick={onClick}>
+        {text}
+    </button>
+);
+
+export default Button
+;
